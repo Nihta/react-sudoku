07e5742e2a5f4c3bac3dbf39526c21b75c16a371
Nihta <trvathin@gmail.com>
2021-08-29 23:58:46 +0700

    Revert "Add highlight and input feature"
    This reverts commit 2bb804994fa948694110a033569829389297af88.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import Sudoku from "./features/sudoku/Sudoku";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Counter } from './features/counter/Counter';
 
 function App() {
   return (
+
     <div className="container my-2">
-      <Sudoku />
+      <Counter />
     </div>
   );
 }
--- src/configs/store.ts
+++ src/configs/store.ts
@@ -1,13 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import sudokuReducer from "../features/sudoku/sudokuSlice";
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
 
 export const store = configureStore({
   reducer: {
-    sudoku: sudokuReducer,
+    counter: counterReducer,
   },
-});
+})
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch
