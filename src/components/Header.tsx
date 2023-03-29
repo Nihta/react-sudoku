6a21c1a8fd94b7a08a9e4a4d6fabf61d3cb84045
Nihta <trvathin@gmail.com>
2023-03-29 18:10:36 +0700

    Add new logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,17 +21,19 @@ export default function Header() {
 
   return (
     <Wrapper>
-      <div />
-      <Content ref={ref}>
-        <NewGameButton onClick={toggle}>New game</NewGameButton>
-        {active && (
-          <NewGameContent
-            callBack={() => {
-              setActive(false);
-            }}
-          />
-        )}
-      </Content>
+      <Container>
+        <Logo>Sudoku</Logo>
+        <Content ref={ref}>
+          <NewGameButton onClick={toggle}>New game</NewGameButton>
+          {active && (
+            <NewGameContent
+              callBack={() => {
+                setActive(false);
+              }}
+            />
+          )}
+        </Content>
+      </Container>
     </Wrapper>
   );
 }
@@ -47,17 +49,51 @@ const Content = styled(NewGameWrapper)`
 `;
 
 const Wrapper = styled.header`
-  height: 60px;
-  margin: 0 0 0 5px;
-  padding: 0 10px;
+  margin-bottom: 5px;
   border-bottom: 1px solid rgba(0, 63, 123, 0.1);
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  justify-content: center;
 
+  height: 60px;
   @media (min-width: 980px) {
     height: 70px;
   }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 0 10px;
+
+  @media (min-width: 801px) {
+    max-width: 801px;
+  }
+
+  @media (min-width: 980px) {
+    max-width: 980px;
+  }
+
+  @media (min-width: 1070px) {
+    max-width: 1070px;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1200px;
+  }
+
+  @media (max-height: 800px) and (min-width: 1070px) {
+    max-width: 1070px;
+  }
+`;
+
+const Logo = styled.div`
+  font-size: 26px;
+  font-weight: 600;
+  color: #344861;
+  cursor: pointer;
 `;
 
 const NewGameButton = styled.div`
