979b5ee6e720e43e419bb577e5bdb2cf3462ebd4
Nihta <trvathin@gmail.com>
2023-03-30 23:37:50 +0700

    Empty project

--- src/common/hooks/reduxHooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-
-import { AppDispatch, RootState } from "../../configs/store";
-
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useTypedDispatch = () => useDispatch<AppDispatch>();
--- src/features/sudoku/hooks/sudokuHooks.ts
+++
@@ -1,8 +0,0 @@
-import { PosType } from "../sudokuSlice";
-import { getKeyCellFromPos } from "../utils";
-import { useTypedSelector } from "../../../common/hooks/reduxHooks";
-
-export const useCell = (pos: PosType) => {
-  const cellKey = getKeyCellFromPos(pos);
-  return useTypedSelector((state) => state.sudoku.cells[cellKey]);
-};
